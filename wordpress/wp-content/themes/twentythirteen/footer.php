f41fc6051d4230954ff3287ed90fc5fdccc07123
YiBanBa <yibanba@hotmail.com>
2017-11-07 21:53:39 +0800

    主题 themes
    删除 Twenty Twelve：Version: 1.2
    更新 Twenty Thirteen：Version: 1.0 --> 2.2

--- wordpress/wp-content/themes/twentythirteen/footer.php
+++ wordpress/wp-content/themes/twentythirteen/footer.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * The template for displaying the footer.
+ * The template for displaying the footer
  *
- * Contains footer content and the closing of the
- * #main and #page div elements.
+ * Contains footer content and the closing of the #main and #page div elements.
  *
  * @package WordPress
  * @subpackage Twenty_Thirteen
@@ -17,7 +16,7 @@
 
 			<div class="site-info">
 				<?php do_action( 'twentythirteen_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentythirteen' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentythirteen' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentythirteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentythirteen' ), 'WordPress' ); ?></a>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
 	</div><!-- #page -->
